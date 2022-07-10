6fa9a6ee85458af4ef46c2466214ba22f7c08074
Derick Nguyen <101297012+noxlumomagna@users.noreply.github.com>
2022-07-10 14:13:02 -0400

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '81b06d64c340c1c8ae17a7e0845e41cf',
+    apiKey: 'get one by registering at themoviedb.org',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
